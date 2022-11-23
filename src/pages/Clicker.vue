2211bc5d22627910f4cb66a2a49304e3f4399b97
Roland Tahker <rolandtahker@gmail.com>
2022-11-23 13:46:57 +0200

    23.11.22

--- src/pages/Clicker.vue
+++ src/pages/Clicker.vue
@@ -1,35 +1,39 @@
 <template>
     <div class="columns">
         <div class="column is-one-third has-text-centered">
-            <b>{{stones}} stones</b>
-            <br>
-            <b>{{cps}} stones per second</b>  
+            <button class="button2" style="width: 40%; height: 8%;">{{stones}} STONES</button><br>
+            <button class="button2" style="width: 40%; height: 8%;">{{sps}} STONES PER SECOND</button>  
             <br>
             <br>
             <img @click="stoneClick()" :class="{'click': clickClass}" src="https://www.animatedimages.org/data/media/1096/animated-click-here-sign-and-button-image-0042.gif">   
         </div>
         <div class="column" style="width: 100%;">
-            <button class="button is-black" style="width: 30%; height: 20%;">CPS SHOP!</button>
+            <button class="button1" style="width: 30%; height: 15%;">SPS SHOP!</button>
             <br> 
             <br> 
-            <button @click="upgrade(0.1, 10)" class="button is-dark" :disabled="stones<10" style="width: 30%;">Buy 0.1 cps for 10 stones</button>        
+            <button @click="upgrade(0.1, 10)" class="button is-dark" :disabled="stones<10" style="width: 30%; height: 10%;">Buy 0.1 sps for 10 stones</button>        
             <br> 
             <br> 
-            <button @click="upgrade(1, 100)" class="button is-dark" :disabled="stones<100" style="width: 30%;">Buy 1 cps for 100 stones</button> 
+            <button @click="upgrade(1, 100)" class="button is-dark" :disabled="stones<100" style="width: 30%; height: 10%;">Buy 1 sps for 100 stones</button> 
             <br>
             <br>
-            <button @click="upgrade2(10, 1000)" class="button is-dark" :disabled="stones<1000" style="width: 30%;">Buy 10 cps for 1000 stones</button> 
+            <button @click="upgrade(10, 1000)" class="button is-dark" :disabled="stones<1000" style="width: 30%; height: 10%;">Buy 10 sps for 1000 stones</button> 
             <br>   
             <br>  
-            <button @click="upgrade3(100, 10000)" class="button is-dark" :disabled="stones<10000" style="width: 30%;">Buy 100 cps for 10000 stones</button> 
+            <button @click="upgrade(100, 10000)" class="button is-dark" :disabled="stones<10000" style="width: 30%; height: 10%;">Buy 100 sps for 10000 stones</button> 
             <br>
             <br>
-            <button @click="upgrade4(1000, 100000)" class="button is-dark" :disabled="stones<100000" style="width: 30%;">Buy 1000 cps for 100000 stones</button>
+            <button @click="upgrade(1000, 100000)" class="button is-dark" :disabled="stones<100000" style="width: 30%; height: 10%;">Buy 1000 sps for 100000 stones</button>
             <br>
             <br>
-            <button @click="upgrade5(1000, 100000)" class="button is-dark" :disabled="stones<1000000" style="width: 30%;">Buy 10000 cps for 1000000 stones</button>         
+            <button @click="upgrade(1000, 100000)" class="button is-dark" :disabled="stones<1000000" style="width: 30%; height: 10%;">Buy 10000 sps for 1000000 stones</button>
+            <br>
+            <br>
+            <br>
+            <button @click="upgrade2(0, 0)" class="button is-dark" :disabled="stones<1" style="width: 30%; height: 10%;">GO BACK TO START</button>       
         </div>
     </div>
+
   
 </template> 
 
@@ -37,7 +41,7 @@
 export default {
     created(){
         setInterval(()=>{
-            this.stones += this.cps;
+            this.stones += this.sps;
             this.stones = parseFloat(this.stones.toFixed(1));
 
         }, 1000);
@@ -46,7 +50,7 @@ export default {
         return {
             clickClass: false,
             stones: 0,
-            cps: 0
+            sps: 0
 
         }
     },
@@ -54,8 +58,8 @@ export default {
         displaystones(){
             return parseFloat(this.stones.toFixed(1));
         },
-        displayCps(){
-            return parseFloat(this.cps.toFixed(1));
+        displaysps(){
+            return parseFloat(this.sps.toFixed(1));
         },
     },
     methods: {
@@ -66,13 +70,24 @@ export default {
                 this.clickClass = false;
             }, 100);
         },
-        upgrade(cps, cost){
+        upgrade(sps, cost){
             if(this.stones >= cost){
                 this.stones -= cost;
-                alert(`Would you like to upgrade?`);
-                this.cps += cps;
+                this.sps += sps;
             }
-        } 
+        },
+        upgrade2(sps, cost){
+            if(this.stones >= cost){         
+            let confirmAction = confirm("Are you sure?");
+                if (confirmAction) {
+                alert("Restarted!");
+                this.sps = 0;
+                this.stones = 0;
+                } else {
+                alert("Keep going!");
+                }
+            }
+        },
     }
 }
 
@@ -84,8 +99,20 @@ img.click {
     transform: scale(0.9);
 }
 
-b {
-    color: darkgrey;
+.button {
+    border-radius : 25px;
+    background-color: rgba(83, 6, 6, 0.699);    
 }
-    
+.button1 {
+    font-size : 40px;
+    background-color: rgba(83, 6, 6, 0.699);
+    color: white;
+    border-radius : 100px;
+}
+.button2 {
+    border-radius : 10px;
+    background-color: rgba(148, 56, 56, 0.699);
+    height : 30px;
+    color: white;
+}   
 </style>
